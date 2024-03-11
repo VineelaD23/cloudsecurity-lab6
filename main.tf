30bffd10645ba381836958cee0c3672867ec4b10
VineelaD23 <97244218+VineelaD23@users.noreply.github.com>
2024-03-11 13:03:09 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 0.13"
+}
+
+terraform {
+  required_version = ">= 0.13"
+}
+
+terraform {
+  required_version = ">= 0.13"
+}
